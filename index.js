2922ebf6d7ffec1140180d2fcb8f09b93876e3c8
5w2g <croizonsacha1@gmail.com>
2026-08-26 04:48:02 +0100

    Debug

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ client.once(Events.ClientReady, () => {
 });
 
 client.on(Events.MessageCreate, async (message) => {
+  console.log(`Message de ${message.author.id}: ${message.content}`);
   if (message.author.bot) return;
   if (message.author.id !== OWNER_ID) return;
   if (!message.content.startsWith(PREFIX)) return;
